395548c9efa565c61b0521b87ac36d797e33543d
chenyongze <sapphire.php@gmail.com>
2020-06-07 23:07:23 +0800

    server

--- express_node_server/app.js
+++ express_node_server/app.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
+const path = require('path')
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+
+app.use(function (req, res, next) {
+    const proxy = req.query.proxy;
+    if (proxy) {
+        req.header.cookie = req.header.cookie + `__proxy__${proxy}`;
+    }
+    next();
 })
 
-app.listen(3000)
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server runing@http://localhost:${port}`)
+})
+
+// app.get('/', function (req, res) {
+//     res.send('Hello World!')
+// })
+
+// app.listen(3000)
